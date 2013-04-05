5387fc4d7dceac9f9a5ec6096385baa16dbec389
unknown <arnab.roychowdhury@WSSYS34.webspiders.com>
2013-04-05 18:23:27 +0530

    Initial commit.

---
+++ PartialViewAspNet/Controllers/ProductController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+using System.Web.Routing;
+using PartialViewAspNet.Models;
+
+namespace PartialViewAspNet.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IProductRepositary Repositary { get; set; }
+
+        protected override void Initialize(RequestContext requestContext)
+        {
+            if (Repositary == null)
+            {
+                Repositary = new ProductRepositary();
+            }
+            base.Initialize(requestContext);
+        }
+
+        //
+        // GET: /Product/
+
+        public ActionResult Index()
+        {
+            IList<Product> products = Repositary.GetAll();
+            return PartialView(products);
+        }
+    }
+}
---
+++ PartialViewAspNet/Models/ProductRepositary.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace PartialViewAspNet.Models
+{
+    public interface IProductRepositary
+    {
+        List<Product> GetAll();    
+    }
+
+    public class ProductRepositary : IProductRepositary
+    {
+        private MobiBookEntities _entities;
+
+        public ProductRepositary()
+        {
+            _entities = new MobiBookEntities();
+        }
+
+        public List<Product> GetAll()
+        {
+            List<Product> products = _entities.Products.Take(50).ToList(); // Get top 50 records.
+            return products;
+        }
+    }
+}
